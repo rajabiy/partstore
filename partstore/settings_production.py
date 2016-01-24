bffc4a917036ebeef67a2d7b67bc659fe9c67f80
aminiy <aminiy@mail.ru>
2016-01-24 19:35:24 +0500

    admin tools

--- partstore/settings_production.py
+++ partstore/settings_production.py
@@ -1,3 +1,37 @@
 from partstore.settings import *
 
 STATIC_ROOT = '/home/part/static/'
+
+DEBUG = False
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'admin_tools.template_loaders.Loader',
+                'django.template.context_processors.request',
+                'django.template.context_processors.debug',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+INSTALLED_APPS = [
+    'admin_tools',
+    'admin_tools.theming',
+    'admin_tools.menu',
+    'admin_tools.dashboard',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'store',
+]
