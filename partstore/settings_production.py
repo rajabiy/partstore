b4226f20f4430454df0167dffa2ce4f7e02afdec
aminiy <aminiy@mail.ru>
2016-02-02 21:59:58 +0500

    todo did

--- partstore/settings_production.py
+++ partstore/settings_production.py
@@ -3,3 +3,12 @@ from partstore.settings import *
 STATIC_ROOT = '/home/part/static/'
 
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/home/parts/partstore/partstore/my.cnf',
+        },
+    }
+}
